9d80c03e71f7bb2a72d010dbcb7973492ab5224d
twinklegenon <111825352+twinklegenon@users.noreply.github.com>
2023-05-25 18:41:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 from keras.models import load_model
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 
 @st.cache_resource
 
